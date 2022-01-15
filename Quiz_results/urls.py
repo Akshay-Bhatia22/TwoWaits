699ad963d94e87a2a8863aadce13e38d351191a0
Akshay-Bhatia22 <dpsvn.aksh06001@gmail.com>
2022-01-15 23:32:49 +0530

    Fix : Return apt response when no options provided

--- Quiz_results/urls.py
+++ Quiz_results/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('attempt/', views.AttemptQuiz.as_view()),
     path('answer/', views.AnswerQuizQuestion.as_view()),
-    path('submit/', views.GenerateResult.as_view())
+    # path('submit/', views.GenerateResult.as_view()),
 ]
--- Quiz_results/views.py
+++ Quiz_results/views.py
@@ -37,40 +37,44 @@ class AnswerQuizQuestion(APIView):
 
     def post(self, request, format=None):
         data = request.data
-        try:
-            # getting instance for foreign relationships
-            quiz_result_id = QuizResult.objects.get(id=data['quiz_result_id'])
-            question_id = QuizQuestion.objects.get(id=data['question_id'])
-            
-            # checking if already answered
-            student_answer_instance = StudentAnswer.objects.filter(quiz_result_id=quiz_result_id).filter(question_id=question_id).first()
-            
-            if not student_answer_instance:
-                # create new answer instance
-                student_answer_instance = StudentAnswer.objects.create(quiz_result_id=quiz_result_id, question_id=question_id)
-
-        except:
-            return Response({'message':'Invalid data entered. Either quiz or question doesn\'t exist'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
-
-        # ------------------------------FOR SINGLE CORRECT OPTIONS-----------------------------------------------------
-        for option in data['options']:
-
-            # getting option isntance for foreign relationship
+        # check if options provided as else no need to register response
+        if data['options']:
+            # Options provided registre response
             try:
-                option_id = Option.objects.get(id=option['option_id'])
+                # getting instance for foreign relationships
+                quiz_result_id = QuizResult.objects.get(id=data['quiz_result_id'])
+                question_id = QuizQuestion.objects.get(id=data['question_id'])
+                
+                # checking if already answered
+                student_answer_instance = StudentAnswer.objects.filter(quiz_result_id=quiz_result_id).filter(question_id=question_id).first()
+                
+                if not student_answer_instance:
+                    # create new answer instance
+                    student_answer_instance = StudentAnswer.objects.create(quiz_result_id=quiz_result_id, question_id=question_id)
+
             except:
-                return Response({'message':'Invalid option_id entered'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-
-            obj = StudentResponse.objects.filter(answer_id=student_answer_instance).filter(option_id=option_id)
+                return Response({'message':'Invalid data entered. Either quiz or question doesn\'t exist'}, status=status.HTTP_400_BAD_REQUEST)
             
-            if obj:
-                # updating existing response
-                obj.update(answer_id=student_answer_instance, option_id=option_id)
-                return Response({'message':'Response updated'}, status=status.HTTP_200_OK)
+            
 
-            # if new response made
-            StudentResponse.objects.create(answer_id=student_answer_instance, option_id=option_id)
-            return Response({'message':'New response registered'}, status=status.HTTP_201_CREATED)
+            # ------------------------------FOR SINGLE CORRECT OPTIONS-----------------------------------------------------
+            for option in data['options']:
+
+                # getting option isntance for foreign relationship
+                try:
+                    option_id = Option.objects.get(id=option['option_id'])
+                except:
+                    return Response({'message':'Invalid option_id entered'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+                obj = StudentResponse.objects.filter(answer_id=student_answer_instance).filter(option_id=option_id)
+                
+                if obj:
+                    # updating existing response
+                    obj.update(answer_id=student_answer_instance, option_id=option_id)
+                    return Response({'message':'Response updated. Question attempted'}, status=status.HTTP_200_OK)
+
+                # if new response made
+                StudentResponse.objects.create(answer_id=student_answer_instance, option_id=option_id)
+                return Response({'message':'New response registered. Question attempted'}, status=status.HTTP_201_CREATED)
+        return Response({'message':'No options provided. Question not attempted'}, status=status.HTTP_204_NO_CONTENT)
 
